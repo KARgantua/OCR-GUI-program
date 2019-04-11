eb3bcaee0ab03474a3570b7c60cf5158896baebc
KARgantua <lsmn0280@gmail.com>
2019-04-11 14:36:47 +0900

    콤보박스에서 언어설정 및 엔진 설정 가능

--- ocr_main.py
+++ ocr_main.py
@@ -13,6 +13,7 @@ def openWindow(self):
     self.window=QtWidgets.QMainWindow()
     self.ui=Ui_CamWindow()
     self.ui.setupUi(self.window)
+    self.ui.setConfig(self.langSel.currentText(), self.engSel.currentText())
     self.ui.captureSig()
     self.window.show()
 
@@ -45,13 +46,24 @@ def capture(self, camid = CAM_ID):
 def readImage(self):
     self.capture()
     self.im = cv2.imread(self.imPath, cv2.IMREAD_COLOR)
-    self.config = ('-l kor --oem 1 --psm 3')
+    self.config = ('-l %s --oem 1 --psm 3' % self.lang)
     self.text = pytesseract.image_to_string(self.im, config=self.config)
     self.camLabel.setText(self.text)
 
 # capture 버튼 시그널
 def captureSig(self):
     self.capBtn.clicked.connect(self.readImage)
+
+# tesseract를 이용할 때 필요한 설정을 세팅하는 함수
+# lang: 인식할 언어
+# engn: 설정할 엔진
+def setConfig(self, lang, engn):
+    self.lang = lang
+    self.engn = engn
+
+# =====================================
+# 적용
+#======================================
 
 # 클래스에 함수 추가
 Ui_MainWindow.cameraSig = cameraSig
@@ -60,6 +72,7 @@ Ui_MainWindow.openWindow = openWindow
 Ui_CamWindow.captureSig = captureSig
 Ui_CamWindow.capture = capture
 Ui_CamWindow.readImage = readImage
+Ui_CamWindow.setConfig = setConfig
 
 if __name__ == "__main__":
     import sys
