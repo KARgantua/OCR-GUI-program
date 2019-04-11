8180719b1d1c4ba90dd843bb28d0dca8453e200d
KARgantua <lsmn0280@gmail.com>
2019-04-11 11:24:57 +0900

    버튼을 사용해 CV로 읽어서 라벨로 띄우기 성공

--- ocr_main.py
+++ ocr_main.py
@@ -2,18 +2,66 @@
 
 from ocr_main_window import *
 from ocr_cam_window import *
+import pytesseract
 
+#===========================================
+# Main Window 에서 실행할 함수 리스트
+#===========================================
+
+# Cam Window 실행
 def openWindow(self):
     self.window=QtWidgets.QMainWindow()
     self.ui=Ui_CamWindow()
     self.ui.setupUi(self.window)
+    self.ui.captureSig()
     self.window.show()
 
-def camera(self):
+# camera 버튼 시그널
+def cameraSig(self):
     self.camBtn.clicked.connect(self.openWindow)
 
-Ui_MainWindow.camera = camera
+#===========================================
+# Cam Window에서 실행할 함수 리스트
+#===========================================
+
+CAM_ID = 0
+def capture(self, camid = CAM_ID):
+    cam = cv2.VideoCapture(camid)
+    imPath = "ocr1.png"
+    if cam.isOpened() == False:
+        print ('cant open the cam (%d)' % camid)
+        return None
+
+    ret, frame = cam.read()
+    if frame is None:
+        print ('frame is not exist')
+        return None
+    
+    # png로 압축 없이 영상 저장 
+    cv2.imwrite(imPath,frame, params=[cv2.IMWRITE_PNG_COMPRESSION,0])
+    cam.release()
+
+    return imPath
+
+# 현재 Cam Label에서 보여주는 이미지 read 후 tesseract 를 사용해서 텍스트로 변환
+def readImage(self):
+    imPath = self.capture()
+    print(imPath)
+    im = cv2.imread(imPath, cv2.IMREAD_COLOR)
+    config = ('-l kor --oem 1 --psm 3')
+    self.text = pytesseract.image_to_string(im, config=config)
+    self.camLabel.setText(self.text)
+
+# capture 버튼 시그널
+def captureSig(self):
+    self.capBtn.clicked.connect(self.readImage)
+
+Ui_MainWindow.cameraSig = cameraSig
 Ui_MainWindow.openWindow = openWindow
+
+Ui_CamWindow.captureSig = captureSig
+Ui_CamWindow.capture = capture
+Ui_CamWindow.readImage = readImage
 
 if __name__ == "__main__":
     import sys
@@ -21,6 +69,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    ui.camera()
+    ui.cameraSig()
     MainWindow.show()
     sys.exit(app.exec_())
